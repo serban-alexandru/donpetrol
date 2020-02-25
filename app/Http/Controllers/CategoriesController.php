3ccba303f9a5a6475896ed15306f655b9978590e
AlexSerbanGit <alexserb2009@yahoo.com>
2020-02-25 21:50:41 +0200

    Product functions

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -13,4 +13,93 @@ class CategoriesController extends Controller
 
     }
 
+    /**
+     * Function that creates a product
+     */
+    public function add(Request $request){
+
+        // data validator
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'price' => 'required|float',
+            'table_number' => 'required|string',
+            'table_part' => 'required|string',
+            'category_id' => 'required|integer|min:1',
+        ]);
+
+        // check if data is valid
+        if($validator->fails()){
+            return redirect()->back()->with('error', 'Invalid data sent!');
+        }
+
+        // create product
+        $product = new Product;
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->table_number = $request->table_number;
+        $product->table_part = $request->table_part;
+        $product->category_id = $request->category_id;
+        $product->save();
+
+        return redirect()->back()->with('success', 'Product added!');
+
+    }
+
+    /**
+     * Function that edits a product
+     */
+    public function edit(Request $request, $product_id){
+
+        // data validator
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'price' => 'required|float',
+            'table_number' => 'required|string',
+            'table_part' => 'required|string',
+        ]);
+
+        // check if data is valid
+        if($validator->fails()){
+            return redirect()->back()->with('error', 'Invalid data sent!');
+        }
+
+        $product = Product::find($product_id);
+
+        // check if product exists
+        if($product){
+            $product->name = $request->name;
+            $product->price = $request->price;
+            $product->table_number = $request->table_number;
+            $product->table_part = $request->table_part;
+            $product->category_id = $request->category_id;
+            $product->save();
+    
+            return redirect()->back()->with('success', 'Product edited!');
+        }
+        
+        // case product does not exist
+        return redirect()->back()->with('error', 'Prduct not found!');
+
+    }
+
+    /**
+     * Function that deletes a product
+     */
+    public function delete($product_id){
+
+        $product = Product::find($product_id);
+
+        // check if product exists
+        if($product){
+
+            $product->delete();
+            return redirect()->back()->with('success', 'Product deleted!');
+
+        }
+
+        // case product does not exist
+        return redirect()->back()->with('error', 'Prduct not found!');
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,33 @@ Auth::routes();
 // home page route
 Route::get('/home', 'HomeController@index')->name('Home');
 
-// categories page route
-Route::get('/categories', 'CategoriesController@index')->name('Categories');
+Route::group(['middleware' => 'auth'], function(){
 
-// products page route
-Route::get('/products', 'ProductsController@index')->name('Products');
+    // categories page route
+    Route::get('/categories', 'CategoriesController@index')->name('Categories');
 
-// order page route
-Route::get('/order', 'OrdersController@index')->name('Order');
+    // add category route
+    Route::post('/add_category', 'CategoriesController@add')->name('Add category');
+
+    // edit category route
+    Route::post('/edit_category/{category_id}', 'CategoriesController@edit')->name('Edit category');
+
+    // delete category route
+    Route::get('/delete_category/{category_id}', 'CategoriesController@delete')->name('Delete category');
+
+    // products page route
+    Route::get('/products', 'ProductsController@index')->name('Products');
+
+    // add product route
+    Route::post('/add_product', 'ProductsController@add')->name('Add product');
+
+    // edit product route
+    Route::post('/edit_product/{category_id}', 'ProductsController@edit')->name('Edit product');
+
+    // delete product route
+    Route::get('/delete_product/{category_id}', 'ProductsController@delete')->name('Delete product');
+
+    // order page route
+    Route::get('/order', 'OrdersController@index')->name('Order');
+
+});
